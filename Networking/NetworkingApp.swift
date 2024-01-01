82f0b67500d74aea78e1fca42fca8a741fcfca96
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-01-01 13:48:41 +0500

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by A'zamjon Abdumuxtorov on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
